fbb2fc48b794f1617fdc97dd2da103073d8adc77
nmarasigan16 <egarsenal16@gmail.com>
2017-06-27 00:19:23 -0500

    Converted to mimemultipart

--- email_script.py
+++ email_script.py
@@ -4,32 +4,41 @@ import sys
 import getpass
 import smtplib
 import csv
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 '''
-This is a script to send emails from python using the email thetatau.media@gmail.com
+This is a script to send emails from python using the email
+thetatau.media@gmail.com
 
 This can be extended to other emails, if not hardcoded
 '''
 
 try:
     if len(sys.argv) < 4:
-        raise Exception('Usage: ./email_script.py <email csv> <subject> <message file> \nPlease read the README for more information')
+        raise Exception("""Usage: ./email_script.py <email csv>
+                        <subject> <message file> \nPlease read
+                        the README for more information""")
 except Exception as e:
     print(e)
     sys.exit()
 
 greetings = ['Hey', 'Hello', 'What\'s up']
 
+
 def connect_to_server():
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.ehlo()
     return server
 
+
 def get_credentials():
     # for other emails, use input('Which email would you like to use?')
     email = 'thetatau.media@gmail.com'
-    password = getpass.getpass('Please input the password to {}: '.format(email))
+    password = getpass.getpass(
+            'Please input the password to {}: '.format(email))
     return {'email': email, 'password': password}
+
 
 def login_to_server(server):
     authenticated = False
@@ -42,9 +51,10 @@ def login_to_server(server):
             print(e)
             print('Please try again')
         else:
-            authenticated=True
-            break;
+            authenticated = True
+            break
     return (authenticated, credentials['email'])
+
 
 def process_message(message_file):
     message = ""
@@ -58,20 +68,28 @@ def process_message(message_file):
     message = message.replace('\n', ' ').replace('\r', ' ')
     return message
 
-def send_message(server, email, receipient, subject, message):
-    #TODO add greeting selection
-    try:
-        email_text = """\
-                From: {0}
-                To: {1}
-                Subject: {2}
 
-                {3} {4},
-                {5}
-                """.format(email, receipient['email'], subject, greeting, receipient['name'], message)
-        server.sendmail(email, receipient, email_text)
+def send_message(server, email, receipient, subject, message):
+    # TODO add greeting selection
+    try:
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = subject
+        msg['From'] = email
+        msg['To'] = receipient
+
+        text = message['text']
+        html = message['html']
+
+        part1 = MIMEText(text, 'plain')
+        part2 = MIMEText(html, 'html')
+
+        msg.attach(part1)
+        msg.attach(part2)
+
+        server.sendmail(email, receipient, msg.as_string())
     except:
         print('Error while sending email to {}'.format(receipient))
+
 
 def process_receipients(receipients_csv):
     receipients = []
@@ -91,6 +109,6 @@ logged_in, email = login_to_server(server)
 if logged_in:
     message = process_message(sys.argv[3])
     receipients = process_receipients(sys.argv[1])
-    for receipient in recepients:
-        send_email(email, receipient, subject, message)
-server.close()
+    for receipient in receipients:
+        send_message(email, receipient, subject, message)
+server.quit()
